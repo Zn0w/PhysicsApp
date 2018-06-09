c82cb14aecf882eb4a51d88f3bf7d9c4ffe93717
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-06-09 18:37:23 +0300

    corrections for the UX

--- gui.py
+++ gui.py
@@ -49,10 +49,10 @@ class GuiHandler:
         height = Label(resultsWindow, text = "Max fly height: " + str(self.mathHandler.maxHeight))
         height.pack()
 
-        self.x = Label(resultsWindow, text = "0 at 0 seconds")
+        self.x = Label(resultsWindow, text = "X: 0 at 0 seconds")
         self.x.pack()
 
-        self.y = Label(resultsWindow, text="0 at 0 seconds")
+        self.y = Label(resultsWindow, text = "Y: 0 at 0 seconds")
         self.y.pack()
 
         info = Label(resultsWindow, text = "Enter moment in seconds to get X and Y position")
@@ -69,8 +69,8 @@ class GuiHandler:
         time = self.moment.get(1.0, END)[:-1]
 
         if float(time) > self.mathHandler.flyTime:
-            self.x.config(text=str(self.mathHandler.getXPosition(self.mathHandler.flyTime)) + " at " + time + " seconds")
-            self.y.config(text=str(self.mathHandler.getYPosition(self.mathHandler.flyTime)) + " at " + time + " seconds")
+            self.x.config(text = "X: " + str(self.mathHandler.getXPosition(self.mathHandler.flyTime)) + " at " + time + " seconds")
+            self.y.config(text = "Y: " + str(self.mathHandler.getYPosition(self.mathHandler.flyTime)) + " at " + time + " seconds")
         else:
-            self.x.config(text=str(self.mathHandler.getXPosition(float(time))) + " at " + time + " seconds")
-            self.y.config(text=str(self.mathHandler.getYPosition(float(time))) + " at " + time + " seconds")
+            self.x.config(text = "X: " + str(self.mathHandler.getXPosition(float(time))) + " at " + time + " seconds")
+            self.y.config(text = "Y: " + str(self.mathHandler.getYPosition(float(time))) + " at " + time + " seconds")
